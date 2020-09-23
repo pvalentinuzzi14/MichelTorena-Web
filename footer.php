64c51fb9788786867eaf297432c0e9bbae62e9db
pvalentinuzzi14 <61996696+pvalentinuzzi14@users.noreply.github.com>
2020-09-22 21:43:33 -0300

    solucion script Bootstrap

--- footer.php
+++ footer.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
-    <head>
-        <title> MichelTorena </title>
-        <meta charset="utf-8">
-        
-        <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1,maximum-sacle=1,minimum-scale=1">
-        <link rel="stylesheet" href="styles.css">
-        <link href="//db.onlinewebfonts.com/c/0a6ee448d1bd65c56f6cf256a7c6f20a?family=Bahnschrift" rel="stylesheet" type="text/css"/>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    </head>
-        
-    <body style="font-family: bahnschrift;">
         <footer class="container-fluid">
             <div class="color-dark container-fluid">
                 <div class="row p-3">
@@ -59,7 +46,10 @@
 
             </div>
         </footer>
-    </body>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        </body>
 </html>
 
 
@@ -68,33 +58,3 @@
 
 
 
-
-                   <!--     <div class="col-1 m-auto"> 
-                            <img src="img/indexFooterLocation.png" alt="item-location" class="p-2 img-fluid">
-                        </div>
-                        <div class="col-3 m-auto ">
-                            <div class="row"> <p class="column__txt">Triunvirato 52</p> </div> 
-                            <div class="row"> <p class="column__txt">Esq. Juan B. Justo</p> </div> 
-                            <div class="row"> <p class="column__txt">Mar del Plata</p> </div> 
-                        </div>
-                        <div class="col-1 m-auto">
-                            <img src="img/indexFooterPhone.png" alt="item-phone" class="p-2 img-fluid">
-                        </div>
-
-                        <div class="col-3 m-auto">
-                            <div class="row"> <p class="column__txt">Telefonos</p> </div> 
-                            <div class="row"> <p class="column__txt">480-6444</p> </div> 
-                            <div class="row"> <p class="column__txt">155 350-291</p> </div> 
-                        </div>
-
-                        <div class="col-1 m-auto"> 
-                            <img src="img/indexFooterMail.png" alt="item-phone"  class="p-2 img-fluid" >
-                        </div>
-
-                        <div class="col-3 m-auto">
-                            <div class="row">
-                              <p>jlmicheltorena@hotmail.com</p>
-                            </div> 
-                        </div>
-                </div> 
-                        
